fe148000c4f18bd04933f384967dcb879bdc8542
nakajima1412 <nakajima1412@icloud.com>
2020-08-13 01:15:02 +0900

    lesson3 end

--- src/components/SalutationName.vue
+++ src/components/SalutationName.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
+    <select @change="$emit('update:salutation', $event.target.value)">
+      <option value="">-</option>
+      <option
+        v-for="item of salutations"
+        :value="item"
+        :key="item"
+        :selected="salutation === item"
+      >
+        {{ item }}
+      </option>
+    </select>
 
+    <input
+      @input="$emit('update:name', $event.target.value)"
+      :value="name"
+      type="text"
+    />
   </div>
 </template>
 
@@ -15,10 +31,19 @@ const salutations = [
 
 export default {
   props: {
-
+    salutation: {
+      type: String,
+      default: ''
+    },
+    name: {
+      type: String,
+      default: ''
+    }
   },
   setup () {
-
+    return {
+      salutations
+    }
   }
 }
 </script>
